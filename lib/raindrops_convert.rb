2b740cec90999c785261bcee464222e7e6567910
Cindy <jl7e12@gmail.com>
2018-05-17 15:45:29 +0100

    Added statement to check empty string in raindrops_convert method and the test passed

--- lib/raindrops_convert.rb
+++ lib/raindrops_convert.rb
@@ -13,7 +13,8 @@ def raindrops_convert(number)
   result_arr = rules.map do |num, string|
     number.divisible_by?(num) ? '' + string : ''
   end
-  result_arr.join('')
+  converted_string = result_arr.join('')
+  converted_string.empty? ? number : converted_string
   # return 'Pling' if n.divisible_by?(3)
   # return 'Plang' if n.divisible_by?(5)
   # return 'Plong' if n.divisible_by?(7)
