5302addc2fe4259000a9c6beaf9e430f0fd55e32
Cindy <jl7e12@gmail.com>
2018-05-17 15:37:24 +0100

    Refactored raindrops_convert method with do..end instead of {}

--- lib/raindrops_convert.rb
+++ lib/raindrops_convert.rb
@@ -3,14 +3,16 @@ def divisible_by?(divisor)
   (self % divisor).zero?
 end
 
-def raindrops_convert(n)
+def raindrops_convert(number)
   rules = {
     3 => 'Pling',
     5 => 'Plang',
     7 => 'Plong'
   }
 
-  result_arr = rules.map { |num, string| n.divisible_by?(num) ? '' + string : '' }
+  result_arr = rules.map do |num, string|
+    number.divisible_by?(num) ? '' + string : ''
+  end
   result_arr.join('')
   # return 'Pling' if n.divisible_by?(3)
   # return 'Plang' if n.divisible_by?(5)
