422e25c44407af7fbaf4d9ee881508bd289f1651
Cindy <jl7e12@gmail.com>
2018-05-17 14:57:33 +0100

    The test failed for returning false

--- spec/raindrops_convert_spec.rb
+++ spec/raindrops_convert_spec.rb
@@ -1,7 +1,11 @@
-require './lib/raindrops_convert'
+require 'raindrops_convert'
 
 describe '#divisible_by?' do
-  it 'returns true if the number is divisiable by the argument' do
+  it 'returns true if the number is divisiable by the divisor' do
     expect(6.divisible_by?(3)).to eq(true)
+  end
+
+  it 'returns false if the number is not divisiable by the divisor' do
+    expect(5.divisible_by?(3)).to eq(false)
   end
 end
